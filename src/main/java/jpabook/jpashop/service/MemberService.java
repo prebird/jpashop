0f7bd8fbf24b57a22134a6040ec479971472cf0e
jyg <yonggyujeong10@gmail.com>
2023-01-23 00:26:31 +0900

    memberService

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,47 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+// JPA는 기본적으로 TRANSACTION으로 써야한다.
+// readOnly = true : 조회에 있어서 성능이 더 좋아진다.
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원가입
+     */
+    @Transactional // 수정가능
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+        return memberRepository.save(member);
+    }
+
+    // 멀티 스레드 환경에서 동시에 가입시 통과될 수 있다.
+    // DB에 unique 제약조건을 추가해서 방어할 수 있다.
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 회원 전체 조회
+     */
+    private List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    private Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
