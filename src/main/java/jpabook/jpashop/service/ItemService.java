b98645106c667f2e536b4869f8255ef5ac248c35
jyg <yonggyujeong10@gmail.com>
2023-01-23 17:56:10 +0900

    상품 기능개발

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    /**
+     * 상품 등록
+     */
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    /**
+     * 상품 조회
+     */
+    public List<Item> findAllItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOneItems(Long findId) {
+        return itemRepository.findOne(findId);
+    }
+
+}
