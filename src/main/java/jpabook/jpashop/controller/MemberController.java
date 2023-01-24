e9cd9092a06c0b18c0ab18059fa383a084c46fb1
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-01-24 17:20:05 +0900

    회원 조회

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -5,10 +5,12 @@ import jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,7 +24,12 @@ public class MemberController {
     }
 
     @PostMapping(value = "/members/new")
-    public String create(@Valid MemberForm memberForm) {
+    public String create(@Valid MemberForm memberForm, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
         Member member = new Member();
         member.setName(memberForm.getName());
         member.setCity(memberForm.getCity());
@@ -31,5 +38,14 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    //api를 만들때는 entity를 반환하면 절대 안됨
+    // entity가 추가되면 api스펙이 변화되기때문
+    @GetMapping("members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
