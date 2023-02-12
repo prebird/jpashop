8071de1d6144c0d0f51734c8f023237e2b94a901
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-02-12 16:07:44 +0900

    컬렉션 초기화는 필드에서 하기

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -22,6 +22,9 @@ public class Member {
     @Embedded
     private Address address;
 
+    // 컬렉션은 필드에서 초기화 하자 주의사항**
+    // null exception 방지
+    // 하이버네이트 컬렉션으로 변경되기 때문에 조회 후 변경하면 안됨
     @OneToMany
     private List<Orders> orders = new ArrayList<>();
 
