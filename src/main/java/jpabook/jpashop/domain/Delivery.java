a7b2f78f267416db80240d0acc88e7e94dffb224
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-02-12 15:36:41 +0900

    Delivery 주소 Address 클래스로 대체

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -14,9 +14,9 @@ public class Delivery {
 
     @OneToOne(mappedBy = "delivery")
     private Orders order;
-    private String city;
-    private String street;
-    private String zipcode;
+
+    @Embedded
+    private Address address;
 
     @Enumerated(EnumType.STRING)    // ORDINAL 절대 쓰면 안되고 STRING으로 해야함
     private DeliveryStatus deliveryStatus;
