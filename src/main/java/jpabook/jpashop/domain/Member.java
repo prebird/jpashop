4eac9b6953fb92b288b98043b0be2374c0ed81f3
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-02-12 15:22:13 +0900

    Member 주소 Address 클래스로 대체

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -17,11 +17,9 @@ public class Member {
     @Column(name = "MEMBER_ID")
     private Long id;
     private String name;
-    
-    // TODO : Address 객체로 분리하기
-    private String city;
-    private String street;
-    private String zipcode;
+
+    @Embedded
+    private Address address;
 
     @OneToMany
     private List<Orders> orders = new ArrayList<>();
