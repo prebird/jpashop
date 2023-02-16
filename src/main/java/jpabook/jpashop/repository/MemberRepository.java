73b5806bae76bf8a904451225fcd15d1d0d31fe0
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-02-16 22:49:15 +0900

    Spring Data JPA 적용 기존의 MemberRepository -> ...Old MemberRepository extends JPA Repository 로변경

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,35 +1,12 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
-@Repository
-public class MemberRepository {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    public Long save(Member member){
-        em.persist(member);
-        return member.getId();
-    }
-
-    public Member findOne(Long id){
-        return em.find(Member.class, id);
-    }
-
-    public List<Member> findAll() { // JPQL : 객체를 대상으로 쿼리
-        return em.createQuery("select m from Member m", Member.class)
-                .getResultList();
-    }
-
-    public List<Member> findByName(String name) {
-        return em.createQuery("select m from Member m where m.name = :name", Member.class)
-                .setParameter("name", name)
-                .getResultList();
-    }
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    // select * from member m where m.name = ...
+    public List<Member> findByName(String name);
 }
+
